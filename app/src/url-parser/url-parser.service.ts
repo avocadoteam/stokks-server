89d15866b783f7afa97887bd1bebb850f2c7fbdf
konst <konstantin.mikheev@icord.cz>
2021-08-02 14:54:44 +0200

    log

--- app/src/url-parser/url-parser.service.ts
+++ app/src/url-parser/url-parser.service.ts
@@ -5,6 +5,7 @@ import * as puppeteer from 'puppeteer';
 @Injectable()
 export class UrlParserService {
   async getArticleImg(link: string): Promise<UrlParseResponse> {
+    console.debug('run url parser');
     const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
 
