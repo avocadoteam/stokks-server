f69f1e23f504fbbdc8b9be02e1bac26d82dc9b5b
woophi <mik.kon6610@gmail.com>
2022-01-20 15:55:05 +0100

    oh yes heroku

--- app/src/main.ts
+++ app/src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
   const port = configService.get<number>('core.port', 3000);
   await app.listen(port, () => {
     console.log(moment().format('DD MM YYYY hh:mm:ss'));
-    console.log('Server listen on port', port, 'in', process.env.NODE_ENV, 'mode');
+    console.log('Server listen on port:', port, 'in', process.env.NODE_ENV, 'mode');
   });
 }
 bootstrap();
