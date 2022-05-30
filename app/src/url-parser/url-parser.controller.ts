1c4262fa461031d805d68363f2aeacfcd1dda38e
Konstantin <konstantin.miheev@sensearena.com>
2022-05-30 08:48:49 +0200

    removed jwt from url-parser

--- app/src/url-parser/url-parser.controller.ts
+++ app/src/url-parser/url-parser.controller.ts
@@ -1,14 +1,12 @@
-import { Body, Controller, Post, Res, UseGuards, UseInterceptors } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { Body, Controller, Post, Res, UseInterceptors } from '@nestjs/common';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
-import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { TransformInterceptor } from 'src/interceptors/transform.interceptor';
 import { UrlParserDto } from './dto/url-parser.model';
 import { UrlParserService } from './url-parser.service';
 
 @ApiTags('Url parse')
 @Controller('api/url-parse')
-@UseGuards(JwtAuthGuard)
 @UseInterceptors(TransformInterceptor)
 export class UrlParserController {
   constructor(private readonly up: UrlParserService) {}
@@ -16,7 +14,6 @@ export class UrlParserController {
   @ApiOperation({ summary: 'Consumes article links and returns imgs from meta og:image' })
   @ApiResponse({ description: 'if link is blank or not valid url', status: 400 })
   @ApiBody({ schema: { example: { links: ['string'] } } })
-  @ApiBearerAuth()
   @Post()
   async getInfo(@Body() model: UrlParserDto, @Res() res: Response) {
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
